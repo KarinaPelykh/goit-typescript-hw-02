a1c4f3ba655b634f6637a2cbde7a6b34e9dea3a3
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2024-02-21 22:54:10 +0100

    ADD: baseTypes

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,7 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+type IsOpen = "enable" | "disable";
+type Union = "string" | "number";
+let union: Union;
+let literal: IsOpen;
